46abe14d326a0fc2f410146c12fad0d54ea20e74
anminam <minamhunmin@gmail.com>
2020-10-26 14:53:10 +0900

    add lion

---
+++ src/Components/Lion.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LionPng from "Assets/images/lion.png";
+
+const Lion = () => {
+  return (
+    <div className="lion">
+      <div>
+        <img src={LionPng} />
+      </div>
+    </div>
+  );
+};
+
+export default Lion;
