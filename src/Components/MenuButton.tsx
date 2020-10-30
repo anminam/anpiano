2ca6e1b0f9b0393f3e0db5e0602a3efe44fbf143
anminam <minamhunmin@gmail.com>
2020-10-30 17:00:04 +0900

    add menubutton

---
+++ src/Components/MenuButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MenuButton = () => {
+  return (
+    <div className="menu-button">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default MenuButton;
--- src/Containers/Header.tsx
+++ src/Containers/Header.tsx
@@ -1,9 +1,11 @@
 import Logo from "Components/Logo";
+import MenuButton from "Components/MenuButton";
 import React from "react";
 
 const Header = () => {
   return (
     <header className="header">
+      <MenuButton />
       <Logo />
     </header>
   );
