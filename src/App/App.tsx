2bcc20b7df9a6d9029e98fed86cfded1f556c93a
anminam <minamhunmin@gmail.com>
2020-10-30 15:53:06 +0900

    update App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import PianoKyes from "Containers/PianoKyes";
-import PianoConfig from "Containers/PianoConfig";
-import LionContainer from "Components/LionConainer";
-import PianoScalePractice from "Containers/PianoScalePractice";
+import MainPage from "Pages/MainPage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="top-wapper">
-          <LionContainer />
-          <PianoKyes />
-        </div>
-        <PianoConfig />
-        <PianoScalePractice />
+        <MainPage />
       </header>
     </div>
   );
