257501a210db6a01fcb75741ca1e72e6f131cb9c
anminam <minamhunmin@gmail.com>
2020-10-30 10:23:35 +0900

    add scale container

---
+++ src/Containers/PianoScalePractice.tsx
@@ -0,0 +1,19 @@
+import Button from "Components/Button";
+import { Utils } from "Core/Utils";
+import usePianoKey from "Hooks/usePianoKey";
+import React from "react";
+
+const PianoScalePractice = () => {
+  const { triggerAttackRelease } = usePianoKey();
+  const handleScale = () => {
+    const list = Utils.getScale5("C3");
+    triggerAttackRelease(list, "8n", 500);
+  };
+  return (
+    <div className="piano_scale_container">
+      <Button text="화음" onClick={handleScale} />
+    </div>
+  );
+};
+
+export default PianoScalePractice;
