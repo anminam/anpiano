528348c1d45f037eb6078fc39084cca017df3808
minamAn <minamhunmin@gmail.com>
2020-10-31 14:18:27 +0900

    side menu

--- src/Containers/SideMenu.tsx
+++ src/Containers/SideMenu.tsx
@@ -1,30 +1,42 @@
 import MenuButton from "Components/MenuButton";
+import { setConfig } from "Core/piano/actions";
 import { RootState } from "Core/Store";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const SideMenu = () => {
   const isSideMenuOpen = useSelector(
     (state: RootState) => state.piano.config.isSideMenuOpen
   );
+
+  const dispatch = useDispatch();
+
   return (
     <div className={`side_menu ${isSideMenuOpen ? "open" : ""}`}>
-      <div className="side_menu__button_wapper">
-        <MenuButton />
+      <div className="side_menu__wapper">
+        <div
+          className="side_menu__background"
+          onClick={() => dispatch(setConfig("isSideMenuOpen", false))}
+        ></div>
+        <div className="side_menu__contents">
+          <div className="side_menu__button_wapper">
+            <MenuButton />
+          </div>
+          <div className="side_menu__title">Menu</div>
+          <ul className="side_menu__list">
+            <li>
+              <button type="button">
+                <h3 className="item-name">Vocal</h3>
+              </button>
+            </li>
+            <li>
+              <button type="button">
+                <h3 className="item-name">Piano</h3>
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="side_menu__title">Menu</div>
-      <ul className="side_menu__list">
-        <li>
-          <button type="button">
-            <h3 className="item-name">Vocal</h3>
-          </button>
-        </li>
-        <li>
-          <button type="button">
-            <h3 className="item-name">Piano</h3>
-          </button>
-        </li>
-      </ul>
     </div>
   );
 };
